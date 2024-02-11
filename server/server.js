efb48e22070575f0d72a59c67f0f95f28fdb6dc9
Sunil Agarwal <130535921+sunil-singularityx@users.noreply.github.com>
2024-02-11 16:18:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,11 +9,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
+    
     console.log(`Server is running on port ${ port }`);
 });
 
 const io = new Server(server, {
     cors: {
+        
         origin: '*',
         methods: ['GET', 'POST']
     }
